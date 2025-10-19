289b83ecd665be6571f74344bf91525e4ad1d122
Ілля Витовтов <izbastenovarsen@gmail.com>
2025-10-19 21:24:48 +0300

    fix3

--- task.sql
+++ task.sql
@@ -1,18 +1,18 @@
 USE ShopDB;
 
 DROP PROCEDURE IF EXISTS get_warehouse_product_inventory;
-
 DELIMITER $$
 
 CREATE PROCEDURE get_warehouse_product_inventory(IN warehouseId INT)
 BEGIN
     SELECT 
-        p.Name AS Name,
-        pi.WarehouseAmount AS WarehouseAmount
+        p.Name AS ProductName,
+        pi.WarehouseAmount AS Quantity
     FROM ProductInventory pi
     JOIN Products p ON pi.ProductID = p.ID
     WHERE pi.WarehouseID = warehouseId;
 END$$
 
 DELIMITER ;
+
 
